b08e429660569eec00e720ee9a8cef5e5a43ad0c
Mary Ballantyne <mballantyne777@gmail.com>
2022-08-24 22:03:01 -0600

    adds artist songs index

---
+++ spec/features/artists/songs/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Artists songs index' do
+  before :each do
+    @prince = Artist.create!(name: 'Prince')
+    @purple = @prince.songs.create!(title: 'Purple Rain', length: 234, play_count: 987644)
+    @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 875, play_count: 34578)
+    end
+
+    it 'shows all of the titles of the songs for the artist' do
+    visit "/artists/#{@prince.id}/songs"
+
+    expect(page).to have_content(@purple.title)
+    expect(page).to have_content(@beret.title)
+  end
+
+  it 'links to each songs show page' do
+    visit "/artists/#{@prince.id}/songs"
+
+    click_on @purple.title
+
+    expect(current_path).to eq("/songs/#{@purple.id}")
+  end
+
+  it 'shows the average song length for the artist' do
+    visit "/artists/#{@prince.id}/songs"
+
+    expect(page).to have_content("Average Song Length for Prince: 554.5")
+  end
+end
